254e8d41507db2a7b1ec4aa590de6ab9093f2a39
PrasanthRajuNPR <prasanthrajunidimoru@gmail.com>
2025-11-13 01:14:06 +0530

    final

--- uni/index.js
+++ uni/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://uniconnect-edu.vercel.app/" 
+      "https://uniconnect-edu.vercel.app" 
     ],
     credentials: true,
   })
